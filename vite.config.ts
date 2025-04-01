8e923a99bb59535e3462bef0f97e7b08b5f12909
Alalmolayki <a.almolayki@gmail.com>
2025-04-01 22:12:58 +0300

    v1.6

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,10 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-
+  build: {
+    rollupOptions: {
+      external: ["/public/images/School_logos/logo7.png", '/public/images/School_logos/logo6.png', '/public/images/School_logos/logo1.png'],
+    },
+  },
 
 });
